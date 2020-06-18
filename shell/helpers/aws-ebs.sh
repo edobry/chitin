e5d052f5f92ab5f82ddeed32b86b4f2870d8e518
edobry <edobry@gmail.com>
2020-06-18 16:48:58 -0400

    add defensive checks and fix typo

--- shell/helpers/aws-ebs.sh
+++ shell/helpers/aws-ebs.sh
@@ -1,6 +1,11 @@
 # watches an EBS volume currently being modified and reports progress
 # args: volumeId
 function watchVolumeModificationProgress() {
+    if [[ -z $1 ]]; then
+        echo "Please supply a volume name!"
+        return 1;
+    fi
+
     watch -n 30 "aws ec2 describe-volumes-modifications --volume-id $1 \
         | jq '.VolumesModifications[0].Progress' | xargs printf '%s%%\n'"
 }
@@ -19,12 +24,17 @@ function checkAZ() {
 # finds the id of an EBS snapshot with the given name
 # args: EBS snapshot name
 function findSnapshot() {
+    if [[ -z $1 ]]; then
+        echo "Please supply a snapshot name!"
+        return 1;
+    fi
+
     SNAPSHOT_ID=$(aws ec2 describe-snapshots --filters "Name=tag:Name,Values=$1" \
       | jq -r '.Snapshots[0].SnapshotId // empty')
 
-      if [[ -z $SNAPSHOT_ID ]]; then return 1; fi
+    if [[ -z $SNAPSHOT_ID ]]; then return 1; fi
 
-      echo $SNAPS
+    echo $SNAPSHOT_ID
 }
 
 # creates an EBS volume in the given AZ with the given name
@@ -32,6 +42,11 @@ function findSnapshot() {
 function createVolume() {
     AZ_NAME=$1
     VOLUME_NAME=$2
+
+    if [[ -z $VOLUME_NAME ]]; then
+        echo "Please supply a volume name!"
+        return 1;
+    fi
 
     if ! checkAZ $AZ_NAME; then return 1; fi
 
@@ -76,6 +91,11 @@ function createVolumeFromSnapshot() {
 # finds the ids of the EBS volumes with the given name
 # args: EBS volume name
 function findVolumesByName() {
+    if [[ -z $1 ]]; then
+        echo "Please supply a volume name!"
+        return 1;
+    fi
+
     aws ec2 describe-volumes --filters "Name=tag:Name,Values=$1" | jq -r '.Volumes[] | .VolumeId'
 }
 
