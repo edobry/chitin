c4dde3b68dbe65b481a4d55dc0213430451f93dc
edobry <edobry@gmail.com>
2020-06-24 10:05:52 -0400

    document helper

--- shell/helpers/k8s.sh
+++ shell/helpers/k8s.sh
@@ -82,6 +82,8 @@ function rds() {
     $DT_DIR/bin/rds.sh $*
 }
 
+# fetches the external url, with port, for a Service with a load balancer configured
+# args: service name
 function getServiceExternalUrl() {
     local svc=$(kubectl get service $1 -o=json)
     local hostname=$(echo "$svc" | jq -r '.status.loadBalancer.ingress[0].hostname')
