4354917d9aa913e8da2ca336c52ae1570c038988
edobry <edobry@gmail.com>
2020-08-11 12:49:29 -0400

    implement getLatestClusterVersion and upgradeEnvironmentClusterVersion

--- shell/helpers/coincollection.sh
+++ shell/helpers/coincollection.sh
@@ -75,3 +75,35 @@ function snapshotNodeState() {
     echo -e "\nResuming node..."
     upDeploy "$deploymentName"
 }
+
+# finds the latest cluster version by querying S3
+function getLatestClusterVersion() {
+    if ! checkAuthAndFail; then return 1; fi
+
+    aws s3 ls s3://chainalysis-dataeng-prod-chainflow-data-release/cluster-data/ | \
+        awk '{ print $2 }' | grep 'v[0-9]\.[0-9]*\.[0-9]*[-a-z0-9]*/' | \
+        sed 's/v//' | sed 's/\///' | sort -V | tail -n1
+}
+
+# upgrades an envrionment's cluster version to either the specified or latest
+# args: environment name, (optional) version
+function upgradeEnvironmentClusterVersion() {
+    local env="$1"
+    local version="$2"
+
+    if [[ -z "$env" ]]; then
+        echo "Please supply an environment name!"
+        return 1;
+    fi
+
+    if [[ -z "$version" ]]; then
+        echo "Fetching latest cluster version..."
+        version=$(getLatestClusterVersion)
+        echo "Found: $latestVersion"
+    fi
+
+    echo "Updating dev ib-backend config..."
+    yq -Pi w "$PROJECT_DIR/dataeng-pipeline/env/$env/configs/ib-backend.yaml" \
+        'ib-backend.cluster.version' "$version"
+    echo "Done!"
+}
