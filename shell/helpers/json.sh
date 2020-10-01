e03d8b3e5a2003448bd936f1e2b134a9d73de02c
edobry <edobry@gmail.com>
2020-10-01 17:58:34 -0400

    add readJSON

--- shell/helpers/json.sh
+++ shell/helpers/json.sh
@@ -13,3 +13,9 @@ function prettyYaml() {
 function prettyYamlMultiple() {
     yq r -PCd'*' -
 }
+
+# reads the value at a certain path from a JSON object
+# args: minified json string, path to read
+function readJSON() {
+    jq -r "$2" <<< $1
+}
