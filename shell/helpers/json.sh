b5565db4eb41fb5287ac851390ee46cc9efb0b12
edobry <edobry@gmail.com>
2020-10-05 17:35:59 -0400

    compact all output

--- shell/helpers/json.sh
+++ shell/helpers/json.sh
@@ -17,7 +17,7 @@ function prettyYamlMultiple() {
 # reads the value at a certain path from a JSON object
 # args: minified json string, path to read
 function readJSON() {
-    jq -r "$2" <<< $1
+    jq -cr "$2" <<< $1
 }
 
 # merges two JSON objects together
