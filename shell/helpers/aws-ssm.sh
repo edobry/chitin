1132742e47d3e44ca4ccda2c70682f2fdc131148
edobry <edobry@gmail.com>
2020-10-07 10:49:42 -0400

    update ssm and coin-collection usages

--- shell/helpers/aws-ssm.sh
+++ shell/helpers/aws-ssm.sh
@@ -1,5 +1,7 @@
 # fetches and decrypts an SSM parameter
 # args: path
 function getSecureParam() {
+    requireArg "a parameter path" $1 || return 1
+
    aws ssm get-parameter --name $1 --with-decryption | jq ".Parameter.Value" | sed "s/\"//g"
 }
