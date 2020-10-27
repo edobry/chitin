f4595f391017f5bd4cb2a7251c1040e2511e929e
edobry <edobry@gmail.com>
2020-10-27 18:00:47 -0400

    implement sparse submodule helper

---
+++ shell/helpers/git.sh
@@ -0,0 +1,26 @@
+# initiates a sparse repository in a subdirectory in order to fetch one specific file
+# args: remote repo url, repo name, relative filepath
+# ref: https://stackoverflow.com/questions/60190759/how-do-i-clone-fetch-or-sparse-checkout-a-single-directory-or-a-list-of-directo/60190760#60190760
+function sparseCheckout() {
+    requireArg "a remote repository" "$1"
+    requireArg "the name of the repository" "$2"
+    requireArg "a filepath" "$3"
+
+    local remoteRepo="$1"
+    local repoName="$2"
+    local filepath="$3"
+
+    mkdir -p "$repoName"
+    pushd "$repoName" > /dev/null
+
+    # init sparse repo
+    git init > /dev/null
+    git config core.sparseCheckout true
+    echo "$filepath" >> .git/info/sparse-checkout
+
+    # fetch single file
+    git remote add origin "$remoteRepo" > /dev/null 2>&1
+    git fetch --depth=1 origin master > /dev/null 2>&1
+    git checkout master > /dev/null 2>&1
+    popd > /dev/null
+}
