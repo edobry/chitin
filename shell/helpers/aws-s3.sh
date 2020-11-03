406fe5e9a4ac7b39c69de7ca766dbcff73db99d3
edobry <edobry@gmail.com>
2020-11-03 13:26:42 -0500

    consolidate s3 and aws-s3

--- shell/helpers/aws-s3.sh
+++ shell/helpers/aws-s3.sh
@@ -10,3 +10,14 @@ function catS3Key() {
     aws s3 cp --quiet $s3Path $localPath
     cat $localPath
 }
+
+# check if the given key in the given s3 bucket exists
+# args: bucket, key
+function s3KeyExists() {
+    checkAuthAndFail || return 1
+
+    requireArg "a bucket name" "$1" || return 1
+    requireArg "a key" "$2" || return 1
+
+    aws s3api head-object --bucket "$1" --key "$2" > /dev/null 2>&1
+}
