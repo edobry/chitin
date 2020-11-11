fdd9fb7d4b54131c9edef23d240ef98ef8faaf55
edobry <edobry@gmail.com>
2020-11-10 19:10:21 -0500

    add read json file helper

--- shell/helpers/json.sh
+++ shell/helpers/json.sh
@@ -14,11 +14,22 @@ function prettyYamlMultiple() {
     yq r -PCd'*' -
 }
 
+function readJSONFile() {
+    requireArg "a filepath" "$1" || return 1
+
+    if [[ ! -f "$1" ]]; then
+        echo "No file exists at the given path!"
+        return 1
+    fi
+
+    cat "$1" | jq -c
+}
+
 # reads the value at a certain path from a JSON object
 # args: minified json string, path to read
 function readJSON() {
-    requireArg "a JSON string" $1 || return 1
-    requireArg "a jq path" $2 || return 1
+    requireArg "a JSON string" "$1" || return 1
+    requireArg "a jq path" "$2" || return 1
 
     jq -cr "$2" <<< $1
 }
@@ -26,8 +37,8 @@ function readJSON() {
 # merges two JSON objects together
 # args: N>2 minified json strings
 function mergeJSON() {
-    requireArg "a JSON string" $1 || return 1
-    requireArg "another JSON string" $2 || return 1
+    requireArg "a JSON string" "$1" || return 1
+    requireArg "another JSON string" "$2" || return 1
 
     jq -s 'add' <<< $@
 }
