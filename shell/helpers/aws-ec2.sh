28b5b0cede2ffe40cf1fd60be427e487eb21264d
edobry <edobry@gmail.com>
2021-01-15 14:01:21 -0500

    implement createKeypair

---
+++ shell/helpers/aws-ec2.sh
@@ -0,0 +1,29 @@
+function createKeypair() {
+    checkAuthAndFail || return 1
+    requireArg 'an environment name' "$1" || return 1
+    requireArg 'a keypair name' "$2" || return 1
+
+    local envName="$1"
+    local keypairName="$2"
+
+    local privKeyFile=$(tempFile)
+
+    echo "Generating keypair '$keypairName'..."
+    aws ec2 create-key-pair --key-name $keypairName | \
+        jq -r '.KeyMaterial' > $privKeyFile
+
+    chmod 600 $privKeyFile
+
+    echo "Determining public key..."
+    local publicKey=$(ssh-keygen -yf $privKeyFile)
+
+    local ssmPath="/$envName/keypairs/$keypairName"
+    echo "Writing keypair to SSM at '$ssmPath'"
+    # echo $ssmPath/public "$publicKey"
+    setSecureParam $ssmPath/private "$publicKey"
+    # echo $ssmPath/private $(cat $privKeyFile)
+    setSecureParam $ssmPath/public $(cat $privKeyFile)
+
+    echo "Cleaning up..."
+    rm $privKeyFile
+}
