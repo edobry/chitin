8fa186ecbbbfa297ac3a77bdfffd592c796e634c
edobry <edobry@gmail.com>
2021-04-02 17:59:46 -0400

    implement aws autoauth

--- shell/helpers/aws-auth.sh
+++ shell/helpers/aws-auth.sh
@@ -1,21 +1,16 @@
+export CA_DT_AWS_ENV_INIT=false
 export CA_DT_AWS_AUTH_INIT=false
 
-function initAwsAuth() {
+function initAwsAuthEnv() {
     if [ "$CA_DT_AWS_AUTH_ENABLED" != true ]; then
         echo "DT aws-auth plugin disabled, set 'CA_DT_AWS_AUTH_ENABLED=true' to enable"
         return 1
     fi
 
-    local programmaticAuth=$(readDTModuleConfig 'aws-auth' '.programmaticAuth')
-    if [[ "$programmaticAuth" == 'true' ]]; then
-        awsInitProgrammaticAuth
-        return 0
-    fi
-
     local embeddedAwsConfig=$CA_DT_DIR/shell/terraform/util/aws/config
 
     # if we're already initialized, we're done
-    ([[ $CA_DT_AWS_AUTH_INIT = "true" ]] && [[ -f $embeddedAwsConfig ]]) && return 0
+    ([[ $CA_DT_AWS_ENV_INIT = "true" ]] && [[ -f $embeddedAwsConfig ]]) && return 0
 
     # set google username
     # export CA_GOOGLE_USERNAME=<name>@chainalysis.com
@@ -33,11 +28,32 @@ function initAwsAuth() {
     sparseCheckout git@github.com:chainalysis/terraform.git $CA_DT_DIR/shell/terraform util/aws/config
     export AWS_CONFIG_FILE=$embeddedAwsConfig
 
-    export CA_DT_AWS_AUTH_INIT=true
+    export CA_DT_AWS_ENV_INIT=true    
+}
+
+function initAutoAwsAuth() {
+    # if we're already initialized, we're done
+    [[ $CA_DT_AWS_ENV_INIT = "true" ]] && return 0
+
+    local programmaticAuth=$(dtReadModuleConfig 'aws-auth' '.programmaticAuth')
+    if [[ "$programmaticAuth" == 'true' ]]; then
+        export CA_DT_AWS_AUTH_INIT=true
+        awsInitProgrammaticAuth
+        return 0
+    fi
+
+    initAwsAuthEnv
+
+    local automaticAuth=$(dtReadModuleConfig 'aws-auth' '.automaticAuth')
+    if [[ "$automaticAuth" == 'true' ]]; then
+        export CA_DT_AWS_AUTH_INIT=true
+        awsInitAutomaticAuth
+        return 0
+    fi
 }
 
 function awsInitProgrammaticAuth() {
-    local programmaticRole=$(readDTModuleConfig 'aws-auth' '.programmaticRole')
+    local programmaticRole=$(dtReadModuleConfig 'aws-auth' '.programmaticRole')
 
     # await authorization complete...
     local roleArn=$(awsGetRoleArn $programmaticRole 2>/dev/null)
@@ -48,6 +64,16 @@ function awsInitProgrammaticAuth() {
 
     aws configure set region $AWS_DEFAULT_REGION
     awsAssumeProgrammaticRoleArn $programmaticRole $roleArn
+}
+
+function awsInitAutomaticAuth() {
+    local profile=$(dtReadModuleConfig 'aws-auth' '.defaultProfile//empty')
+    if [[ -z $profile ]]; then
+        dtLog "automaticAuth enabled, but defaultProfile not set!"
+        return 1
+    fi
+    # echo "authorizing $profile..."
+    awsAuth $profile
 }
 
 # prints your full identity if authenticated, or fails
@@ -142,7 +168,7 @@ function awsOrg() {
 # checks if you're authenticated, triggers authentication if not,
 # and then assumes the provided role
 function awsAuth() {
-    initAwsAuth || return 1
+    initAwsAuthEnv || return 1
 
     requireArg "a profile name" $1 || return 1
 
@@ -163,7 +189,7 @@ alias aws-auth=awsAuth
 # run a command with a specific AWS profile
 # args: profile name
 function withProfile() {
-    initAwsAuth || return 1
+    initAwsAuthEnv || return 1
 
     requireArg "a profile name" $1 || return 1
     local profile="$1"
