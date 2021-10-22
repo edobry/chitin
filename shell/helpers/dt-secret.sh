7b90caaba378d7af770b68a2bea81f7764bf6e78
edobry <edobry@gmail.com>
2021-10-22 14:16:57 -0400

    add dtSecret helpers

---
+++ shell/helpers/dt-secret.sh
@@ -0,0 +1,21 @@
+CA_DT_SECRETS_MODULE_NAME=dtSecret
+
+function dtSecretGet() {
+    requireArg "a secret name" "$1" || return 1
+
+    local commandField=command
+
+    local secretCommand
+    secretCommand=$(dtReadModuleConfigField "$CA_DT_SECRETS_MODULE_NAME" command)
+    if [[ $? -ne 0 ]]; then
+        echo "$secretCommand"
+        return 1
+    fi
+    
+    if [[ -z "$secretCommand" ]]; then
+        dtLog "'$CA_DT_SECRETS_MODULE_NAME' config section not initialized, please set '$commandField' field!"
+        return 1
+    fi
+
+    $secretCommand "$1"
+}
