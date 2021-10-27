ff86fbc7eec6a4975424fd2c5ddcb5562a940931
edobry <edobry@gmail.com>
2021-10-27 12:58:06 -0400

    nest aws modules

--- shell/helpers/aws-asg.sh
+++
@@ -1,8 +0,0 @@
-# gets the tags for the ASG with the given name
-# args: ASG name
-function awsAsgGetTags() {
-    requireArg "an ASG name" "$1" || return 1
-
-    aws autoscaling describe-auto-scaling-groups | jq -r --arg asgName "$1" \
-        '.AutoScalingGroups[] | select(.AutoScalingGroupName == $asgName) | .Tags[] | "\(.Key): \(.Value)"'
-}
--- shell/helpers/aws-net.sh
+++
@@ -1,5 +0,0 @@
-function awsVpcNatIp() {
-    checkAuthAndFail || return 1
-
-    aws ec2 describe-nat-gateways | jq -r '.NatGateways[] | select(.State == "available") | .NatGatewayAddresses[].PublicIp'
-}
