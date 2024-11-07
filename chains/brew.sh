fbba266ec64eba99f6305566d27e51105bf5aaea
Eugene Dobry <edobry@gmail.com>
2024-11-07 15:53:45 -0500

    add brew helpers

---
+++ chains/brew.sh
@@ -0,0 +1,18 @@
+function brewCheck() {
+    requireArg "a package name" "$1" || return 1
+    local name="$1"; shift
+
+    brew ls -1 "$name" $* &>/dev/null
+}
+
+function brewCheckFormula() {
+    requireArg "a formula name" "$1" || return 1
+
+    brewCheck "$1" --formula
+}
+
+function brewCheckCask() {
+    requireArg "a cask name" "$1" || return 1
+
+    brewCheck "$1" --cask
+}
