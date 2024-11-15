e9e8fc08d17b28e70a7feb7ba81fcf3b6afdee74
Eugene Dobry <eugene@dobry.me>
2024-11-14 21:03:07 -0500

    parameterize chiSecret

--- chains/secret.sh
+++ chains/secret.sh
@@ -1,20 +1,20 @@
-CHI_SECRETS_CHAIN_NAME=chiSecret
+export CHI_SECRETS_CHAIN_NAME="core:chiSecret"
 
 # retrieves a secret with the given name from the secret store
 function chiSecretGet() {
     requireArg "a secret name" "$1" || return 1
 
-    local commandField=command
+    local commandField="tool"
 
     local secretCommand
-    secretCommand=$(chiConfigChainReadField "$CHI_SECRETS_CHAIN_NAME" command)
+    secretCommand=$(chiConfigChainReadField "$CHI_SECRETS_CHAIN_NAME" "$commandField")
     if [[ $? -ne 0 ]]; then
         echo "$secretCommand"
         return 1
     fi
     
     if [[ -z "$secretCommand" ]]; then
-        chiLog "'$CHI_SECRETS_CHAIN_NAME' config section not initialized, please set '$commandField' field!"
+        chiLog "config section not initialized, please set '$commandField' field!" "$CHI_SECRETS_CHAIN_NAME"
         return 1
     fi
 
